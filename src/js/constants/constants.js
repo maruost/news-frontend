1f32ba90e1cc271887db8c35113d8893f74149cd
unknown <ostapenko.m.a@yandex.ru>
2020-10-29 13:45:10 +0300

    constants added to constants folder

---
+++ src/js/constants/constants.js
@@ -0,0 +1,61 @@
+const errorMessages = {
+  empty: "Это обязательное поле",
+  wrongLength: "Должно быть от 2 до 30 символов",
+  wrongType: "Здесь должна быть ссылка",
+  printEmail: "Введите действующий e-mail",
+  missKeyWord: "Нужно ввести ключевое слово",
+};
+
+// popups
+const registrationPopup = document.querySelector("#popup-signup");
+const signUpSuccessPopup = document.querySelector("popup-success");
+const authPopup = document.querySelector("#popup-signin");
+
+// popups elems: open buttons
+const registrationButton = document.querySelector("#reg-header-btn");
+const authButton = document.querySelector("#auth-header-btn");
+
+// popups elems: close buttons
+const closeAuthPopup = document.querySelector("#popup-signin-close");
+const closeSuccessPopup = document.querySelector("#popup-success-close");
+const closeRegistrationPopup = document.querySelector("#popup-signup-close");
+
+// forms and forms elems
+const signInForm = document.forms.login;
+const signUpForm = document.forms.registration;
+const searchForm = document.forms.search;
+
+// templates
+const cardTemplate = document.querySelector("#news-card-template").content;
+
+// buttons
+const showMoreButton = document.querySelector(".search-results__button");
+
+// containers and blocks
+const header = document.querySelector(".header");
+const newsCardsContainer = document.querySelector(".news-list");
+const searchResultsContainer = document.querySelector(".search-results");
+const foundResults = document.querySelector(".search-results__container");
+const notFound = document.querySelector(".not-found");
+
+export {
+  errorMessages,
+  registrationPopup,
+  signUpSuccessPopup,
+  authPopup,
+  registrationButton,
+  authButton,
+  closeAuthPopup,
+  closeSuccessPopup,
+  closeRegistrationPopup,
+  signInForm,
+  signUpForm,
+  searchForm,
+  cardTemplate,
+  showMoreButton,
+  header,
+  newsCardsContainer,
+  searchResultsContainer,
+  foundResults,
+  notFound,
+};
