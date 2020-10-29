e1843869700a23fa20463b82df79862fb46ae76a
unknown <ostapenko.m.a@yandex.ru>
2020-10-29 20:57:11 +0300

    added classes to get articles. user info, delete cards

--- src/js/api/MainApi.js
+++ src/js/api/MainApi.js
@@ -7,6 +7,7 @@ export default class MainApi {
   signup = ({ partOfUrl, email, password, name }) => {
     return fetch(`${this.url}${partOfUrl}`, {
       method: "POST",
+      credentials: "include",
       headers: this.headers,
       body: JSON.stringify({
         email: email,
@@ -15,6 +16,68 @@ export default class MainApi {
       }),
     }).then((res) => this._checkStatus(res));
   };
+
+  signin = ({ partOfUrl, email, password }) => {
+    return fetch(`${this.url}${partOfUrl}`, {
+      method: "POST",
+      credentials: "include",
+      headers: this.headers,
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    }).then((res) => this._checkStatus(res));
+  };
+
+  getUserData = ({ partOfUrl }) => {
+    return fetch(`${this.url}users/${partOfUrl}`, {
+      method: "GET",
+      credentials: "include",
+      headers: this.headers,
+    }).then((res) => this._checkStatus(res));
+  };
+
+  getArticles = ({ partOfUrl }) => {
+    return fetch(`${this.url}${partOfUrl}`, {
+      method: "GET",
+      credentials: "include",
+      headers: this.headers,
+    }).then((res) => this._checkStatus(res));
+  };
+
+  saveArticle = ({
+    partOfUrl,
+    keyword,
+    title,
+    text,
+    date,
+    source,
+    link,
+    image,
+  }) => {
+    return fetch(`${this.url}${partOfUrl}`, {
+      method: "POST",
+      credentials: "include",
+      headers: this.headers,
+      body: JSON.stringify({
+        keyword: keyword,
+        title: title,
+        text: text,
+        date: date,
+        source: source,
+        link: link,
+        image: image,
+      }),
+    }).then((res) => this._checkStatus(res));
+  };
+
+  removeArticle = ( {partOfUrl, cardID} ) => {
+    return fetch(`${this._url}${partOfUrl}/${cardID}`, {
+      method: "DELETE",
+      credentials: "include",
+      headers: this.headers,
+    }).then((res) => this._checkStatus(res));
+  }
 
   _checkStatus = (res) => {
     if (!res.ok) {
