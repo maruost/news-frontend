209326e583a49061f743e8a92c865d9d25029ea8
unknown <ostapenko.m.a@yandex.ru>
2020-10-31 16:26:23 +0300

    commented code deleted

--- src/js/utils/keysSorting.js
+++ src/js/utils/keysSorting.js
@@ -21,19 +21,3 @@ function byField(field) {
 }
 
 export { keysObj, byField };
-
-// res.data.forEach((item) => {
-//   keywords.push(item.keyword);
-// });
-// console.log(keywords)
-// let uniqueKeys = new Set(keywords);
-// uniqueKeys.forEach((key) => {
-//   let freq = keywords.lastIndexOf(key) - keywords.indexOf(key) + 1;
-//   sortedKeys.push({ keyword: key, frequency: freq })
-// });
-// console.log(sortedKeys);
-// function byField (field) {
-//   return function (a, b) {
-//       return a.field > b.field ? 1 : -1;
-//   };
-// }
